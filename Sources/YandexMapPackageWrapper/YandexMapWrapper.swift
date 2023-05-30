71a30f039505904673f4373bd16f603724385cd0
Vladimir Goncharov <vladimir1631@yandex.ru>
2023-05-30 23:35:12 +0400

    Update YandexMapWrapper.swift

--- Sources/YandexMapPackageWrapper/YandexMapWrapper.swift
+++ Sources/YandexMapPackageWrapper/YandexMapWrapper.swift
@@ -2,8 +2,8 @@ import Foundation
 import YandexMapsMobile
 
 public final class YandexMapWrapper {
-    static func run(apiKey: String,
-                    locale: String?) {
+    public static func run(apiKey: String,
+                           locale: String?) {
         YMKMapKit.setApiKey(apiKey)
         YMKMapKit.setLocale(locale)
         YMKMapKit.sharedInstance()
