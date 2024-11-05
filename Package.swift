feb4ee73649591de9385ae4902369212e3420ad3
Vladimir Goncharov <vladimir1631@yandex.ru>
2024-11-05 19:20:11 +0400

    4.7.0

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(
             url: "https://github.com/c-villain/YandexMapsMobileLite",
-            .exact("4.6.1")
+            .exact("4.7.0")
         ),
     ],
     targets: [
